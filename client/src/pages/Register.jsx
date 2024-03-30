ccf319afafab5b5137c5d702399a7b5dc9ddab8a
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-30 23:01:37 +0300

    finalize register page and it's functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const navigate = useNavigate();
+
+  axios.defaults.withCredentials = true;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Clear previous messages
+    setError("");
+    setSuccess("");
+
+    try {
+      const response = await axios.post("http://localhost:3000/student/register", {
+        username,
+        email,
+        password,
+        role,
+      });
+      console.log(response);
+      if (response.data && response.data.registered) {
+        setSuccess("Registered successful");
+
+      } else {
+        setError("Failed to Register");
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Failed to Register");
+    }
+  };
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <form className="max-w-full h-full p-10 rounded-xl mx-auto bg-slate-800">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-full h-full p-10 rounded-xl mx-auto bg-slate-800"
+      >
+        {error && (
+          <p className="text-red-500 text-sm mb-3 text-center">{error}</p>
+        )}
+        {success && (
+          <p className="text-green-500 text-sm mb-3 text-center">{success}</p>
+        )}
         <p className=" text-white text-[30px] text-center font-bold">
           Register Student
         </p>
@@ -18,6 +64,8 @@ const Register = () => {
           <input
             type="name"
             id="name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 min-w-[400px] outline-none focus:outline-none active:outline-none"
             placeholder=""
             required
@@ -33,6 +81,8 @@ const Register = () => {
           <input
             type="email"
             id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 min-w-[400px] outline-none focus:outline-none active:outline-none"
             placeholder="name@flowbite.com"
             required
@@ -48,6 +98,8 @@ const Register = () => {
           <input
             type="password"
             id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 min-w-[400px] outline-none focus:outline-none active:outline-none"
             required
           />
@@ -59,9 +111,13 @@ const Register = () => {
           >
             Your Role
           </label>
-          <select className="w-[400px] text-end py-3 rounded outline-none focus:outline-none active:outline-none">
-            <option value="admin">Admin</option>
-            <option value="client">Student</option>
+          <select
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            className="w-[400px] text-start py-3 rounded outline-none focus:outline-none active:outline-none"
+          >
+            {/* <option value="admin">Admin</option> */}
+            <option value="student">Student</option>
           </select>
         </div>
         <div className="flex justify-between items-center text-white px-4 py-3">
