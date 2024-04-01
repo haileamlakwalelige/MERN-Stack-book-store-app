b411233ee880f65fe3f6830d0ca6ba12583632b6
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-02 02:54:39 +0300

    update the auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Admin = require("../models/admin");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const studentRouter = require('../models/Student');
 
 const router = express.Router();
 
@@ -27,9 +28,25 @@ router.post("/login", async (req, res) => {
             console.error("Error:", error);
             return res.status(500).json({ message: "Internal Server Error" });
         }
-    } else if (role === "client") {
-        // Handle client login
-        return res.status(501).json({ message: "Client login not implemented yet" });
+    } else if (role === "student") {
+        try {
+            const student = await studentRouter.findOne({ username });
+            if (!student) {
+                return res.status(401).json({ message: "Student not Registered" });
+            }
+
+            const validPassword = await bcrypt.compare(password, student.password);
+            if (!validPassword) {
+                return res.status(401).json({ message: "Wrong Password" });
+            }
+
+            const token = jwt.sign({ username: student.username, role: 'student' }, process.env.Student_Key);
+            res.cookie("token", token, { httpOnly: true, secure: true });
+            return res.json({ login: true, role: 'student' });
+        } catch (error) {
+            console.error("Error:", error);
+            return res.status(500).json({ message: "Internal Server Error" });
+        }
     } else {
         return res.status(400).json({ message: "Invalid role" });
     }
