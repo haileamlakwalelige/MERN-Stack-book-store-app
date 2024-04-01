7c6f5d9a6890b12a58932bdecc4a3b0b114290e9
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-02 02:59:53 +0300

    update the navbar section

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({roles}) => {
+  console.log("roles", roles)
   return (
     <div className="top-0 sticky w-screen overflow-hidden">
       <div className="flex justify-between items-center bg-slate-800 h-[90px] py-6 text-white px-2 sm:px-6 md:px-12 lg:px-20 border-b-2 border-purple-500">
@@ -15,26 +16,42 @@ const Navbar = () => {
               Books
             </p>
           </Link>
-          <div className="flex gap-4 justify-center items-center font-semibold">
-            <Link to="/books">
-              <p className="hover:border-2 border-purple-500 px-10 py-2 rounded">
-                Add Books
-              </p>
-            </Link>
-            <Link to="/books">
-              <p className="hover:border-2 border-purple-500 px-10 py-2 rounded">
-                Add Student
-              </p>
-            </Link>
-            <Link to="/books">
-              <p className="hover:border-2 border-purple-500 px-10 py-2 rounded">
-                Dashboard
-              </p>
-            </Link>
-          </div>
-          <Link to="/login">
-            <p className="bg-white text-slate-800 px-10 py-2 rounded hover:bg-slate-800 hover:border-white hover:text-white hover:border-2">Login</p>
-          </Link>
+
+          {roles === "admin" ? (
+            <div className="flex gap-4 justify-center items-center font-semibold">
+              <Link to="/books">
+                <p className="hover:border-2 border-purple-500 px-10 py-2 rounded">
+                  Add Books
+                </p>
+              </Link>
+              <Link to="/books">
+                <p className="hover:border-2 border-purple-500 px-10 py-2 rounded">
+                  Add Student
+                </p>
+              </Link>
+              <Link to="/books">
+                <p className="hover:border-2 border-purple-500 px-10 py-2 rounded">
+                  Dashboard
+                </p>
+              </Link>
+            </div>
+          ) : null}
+
+          {
+            (roles == ""  ? (
+              <Link to="/login">
+                <p className="bg-white text-slate-800 px-10 py-2 rounded hover:bg-slate-800 hover:border-white hover:text-white hover:border-2">
+                  Login
+                </p>
+              </Link>
+            ) : (
+              <Link to="/">
+                <p className="bg-white text-slate-800 px-10 py-2 rounded hover:bg-slate-800 hover:border-white hover:text-white hover:border-2">
+                  LogOut
+                </p>
+              </Link>
+            ))
+          }
         </div>
       </div>
     </div>
