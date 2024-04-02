c23202235c80455ff182c19f944af5cec5dd6b95
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-02 22:45:43 +0300

    update home page to have login and logout functionality based on their value

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import books from '../assets/shop.png';
+import React, { useEffect } from "react";
+import books from "../assets/shop.png";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({ setRoles }) => {
+  axios.defaults.withCredentials = true;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/auth/verify");
+        if (res.data.login) {
+          setRoles(res.data.roles);
+        } else {
+          setRoles("");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+
   return (
     <div>
-        <div className='grid grid-cols-1 md:grid-cols-2  justify-between items-center px-2 sm:px-6 md:px-12 lg:px-20'>
-            <div>
-                <p className='text-yellow-600 font-bold text-[50px] font-serif'>Book Shop</p>
-                <p className='text-yellow-600 pt-4 text-lg flex'>Browse the collection of our best top interesting <br/> Books. you will definitely find what you are <br/> looking for.</p>
-            </div>
-            <div>
-                <img src={books} alt='' className='min-h-full'/>
-            </div>
+      <div className="grid grid-cols-1 md:grid-cols-2  justify-between items-center px-2 sm:px-6 md:px-12 lg:px-20">
+        <div>
+          <p className="text-yellow-600 font-bold text-[50px] font-serif">
+            Book Shop
+          </p>
+          <p className="text-yellow-600 pt-4 text-lg flex">
+            Browse the collection of our best top interesting <br /> Books. you
+            will definitely find what you are <br /> looking for.
+          </p>
         </div>
+        <div>
+          <img src={books} alt="" className="min-h-full" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
